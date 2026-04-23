f91feb3f6e5f60903c3607ae7dc2570b3be2a605
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-23 11:36:11 -0500

    lifetime issues

--- src/Soenneker.Cursor.CloudAgents.Runners.OpenApiClient/Startup.cs
+++ src/Soenneker.Cursor.CloudAgents.Runners.OpenApiClient/Startup.cs
@@ -21,10 +21,10 @@ public static class Startup
     public static IServiceCollection SetupIoC(this IServiceCollection services)
     {
         services.AddHostedService<ConsoleHostedService>()
-                .AddScoped<IFileOperationsUtil, FileOperationsUtil>()
-                .AddRunnersManagerAsScoped()
-                .AddKiotaUtilAsScoped()
-                .AddCloudflareDownloaderAsScoped();
+                .AddSingleton<IFileOperationsUtil, FileOperationsUtil>()
+                .AddRunnersManagerAsSingleton()
+                .AddKiotaUtilAsSingleton()
+                .AddCloudflareDownloaderAsSingleton();
 
         return services;
     }
